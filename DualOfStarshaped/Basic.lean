78a9c68eb0684c2ce6fac4304a806b13b23e5ca7
Mmmm1ch43l <lolralf1@gmail.com>
2025-05-16 01:22:36 +0200

    Initial Commit

---
+++ DualOfStarshaped/Basic.lean
@@ -0,0 +1,4 @@
+import Mathlib.Analysis.Convex.Basic
+import Mathlib.Analysis.Convex.Star
+
+def starshapedPolygon := "lol"
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import DualOfStarshaped
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
