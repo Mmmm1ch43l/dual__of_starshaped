80041ca5293813fd56c9cc4bc14d0615083f0656
Mmmm1ch43l <lolralf1@gmail.com>
2025-05-17 00:41:33 +0200

    started on definitions and theorem statements

--- DualOfStarshaped/Basic.lean
+++ DualOfStarshaped/Basic.lean
@@ -1,4 +1,28 @@
-import Mathlib.Analysis.Convex.Basic
-import Mathlib.Analysis.Convex.Star
+import Mathlib.Analysis.Calculus.ContDiff.Defs
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
+import Mathlib.MeasureTheory.Integral.IntervalIntegral.Basic
 
-def starshapedPolygon := "lol"
+open Real
+
+def admissibleFunction (f : ℝ → ℝ) : Prop :=
+  (ContDiff ℝ (⊤ : ℕ∞) f) ∧
+  (∀ x : ℝ, f x > 0 ∧ f (x + 2*π) = f x) ∧
+  (∀ q p : ℤ, Int.gcd p q = 1 → (∀ x : ℝ, deriv f x = (p : ℝ)/(q : ℝ) → f x ≥ (p + 1)/q))
+
+def symmetricAdmissibleFunction (f : ℝ → ℝ) : Prop :=
+    admissibleFunction f ∧
+    (∀ x : ℝ, f (x + π) = f x)
+
+lemma nonOptimality (f : ℝ → ℝ) (hf : symmetricAdmissibleFunction f) :
+    ∃ g : ℝ → ℝ, symmetricAdmissibleFunction g ∧
+    ∫ (x : ℝ) in (0 : ℝ)..2 * Real.pi, g x < ∫ (x : ℝ) in (0 : ℝ)..2 * Real.pi, f x :=
+  sorry
+
+theorem systolicInequality (f : ℝ → ℝ) (hf : symmetricAdmissibleFunction f) :
+    ∫ (x : ℝ) in (0 : ℝ)..2 * Real.pi, f x > 4/3 :=
+  sorry
+
+theorem systolicInequalityIsSharp :
+    ∀ ε > 0, ∃ f : ℝ → ℝ, symmetricAdmissibleFunction f ∧
+    ∫ (x : ℝ) in (0 : ℝ)..2 * Real.pi, f x < 4/3 + ε :=
+  sorry
--- Main.lean
+++ Main.lean
@@ -1,4 +1,1 @@
 import DualOfStarshaped
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
