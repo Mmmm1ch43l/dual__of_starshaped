482f27bdb654ac47ad7331c986e0f5e0641fb978
Mmmm1ch43l <lolralf1@gmail.com>
2025-05-16 17:08:26 +0200

    started on definition

--- DualOfStarshaped/Basic.lean
+++ DualOfStarshaped/Basic.lean
@@ -1,4 +1,9 @@
-import Mathlib.Analysis.Convex.Basic
-import Mathlib.Analysis.Convex.Star
+import Mathlib.Algebra.Ring.Periodic
+import Mathlib.Data.Real.Basic
+import Mathlib.Analysis.Calculus.ContDiff.Defs
 
-def starshapedPolygon := "lol"
+-- want to define a class for functions that are admissible, i.e. smooth functions from ℝ to ℝ which are positive and 2pi periodic
+def admissableFunction (f : ℝ → ℝ) : Prop :=
+  ContDiff ℝ ⊤ f ∧
+  Periodic f (2 * π) ∧
+  ∀ x : ℝ, f x > 0
