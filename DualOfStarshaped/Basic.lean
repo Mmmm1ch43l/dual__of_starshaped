5a58da273eade6462ba2ab67416763f4758c0bf3
Mmmm1ch43l <lolralf1@gmail.com>
2025-05-20 19:44:23 +0200

    various improvements

--- DualOfStarshaped/Basic.lean
+++ DualOfStarshaped/Basic.lean
@@ -1,91 +1,122 @@
 import Mathlib
 
-open Real
+namespace polygons
+  open Rat
 
-noncomputable def angle (u v : ℝ × ℝ) : ℝ :=
-  arccos ((u.1 * v.1 + u.2 * v.2) / (norm u * norm v)) * (if (u.1 * v.2 - u.2 * v.1) < 0 then -1 else 1)
+  def dot (u v : ℚ × ℚ) : ℚ :=
+    u.1 * v.1 + u.2 * v.2
 
-lemma rightAngle (u v : ℝ × ℝ)(o : u.1 * v.1 + u.2 * v.2 = 0)(p: u.1 * v.2 - u.2 * v.1 > 0) : angle u v = π/2 :=
-  by
-  unfold angle
-  rw [o, zero_div]
-  simp only [Real.arccos_zero]
-  rw [if_neg (not_lt_of_gt p)]
-  norm_num
+  def cross (u v : ℚ × ℚ) : ℚ :=
+    u.1 * v.2 - u.2 * v.1
 
-def admissibleFunction (f : ℝ → ℝ) : Prop :=
-  (ContDiff ℝ (⊤ : ℕ∞) f) ∧
-  (∀ x : ℝ, f x > 0 ∧ f (x + 2*π) = f x) ∧
-  (∀ p q : ℤ, Int.gcd p q = 1 → (∀ x : ℝ, angle (p, q) (cos x * deriv f x - sin x * f x, sin x * deriv f x + cos x * f x) = π/2 → (p * cos x + q * sin x) * f x ≥ 1))
+  def rotateRight (u : ℚ × ℚ) : ℚ × ℚ :=
+    (u.2, -u.1)
 
-def symmetricAdmissibleFunction (f : ℝ → ℝ) : Prop :=
-  admissibleFunction f ∧
-  (∀ x : ℝ, f (x + π) = f x)
+  structure starshaped where
+    n : ℕ
+    nonEmpty : n ≥ 3
+    neZero : NeZero n := ⟨Nat.ne_of_gt (Nat.lt_of_lt_of_le (by norm_num) nonEmpty)⟩
+    succ (i : Fin n) : Fin n := Fin.ofNat' n (i + 1)
+    succ2 (i : Fin n) : Fin n := Fin.ofNat' n (i + 2)
+    succ3 (i : Fin n) : Fin n := Fin.ofNat' n (i + 3)
+    vertices : Fin n → ℚ × ℚ
+    isStarshaped : ∀ i j : Fin n, j = (i + 1) % n → cross (vertices i) (vertices j) > 0
+    simple : ∃! i : Fin n, (vertices i).1 ≤ 0 ∧ (vertices (succ i)).1 > 0
 
-noncomputable def integrate (f : ℝ → ℝ) : ℝ :=
-  ∫ (x : ℝ) in (0 : ℝ)..2 * Real.pi, ((f x)^2)/2
+  def convex (P : starshaped) : Prop :=
+    ∀ i j k : Fin P.n, j = (i + 1) % P.n ∧ k = (i + 2) % P.n →
+    cross (P.vertices j - P.vertices i) (P.vertices k - P.vertices j) > 0
 
-structure starshapedPolygon where
-  n : ℕ
-  nonEmpty : n ≥ 3
-  vertices : Fin n → ℚ × ℚ
-  isStarshaped : ∀ i j : Fin n, j = (i + 1) % n → angle ((vertices i).1, (vertices i).2) ((vertices j).1, (vertices j).2) > 0
-  simple : ∑ i : Fin n, ∑ j : Fin n, (if j = (i + 1) % n then angle ((vertices i).1, (vertices i).2) ((vertices j).1, (vertices j).2) else 0) = 2 * π
+  def area (P : starshaped) : ℚ :=
+    ∑ i : Fin P.n, (cross (P.vertices i) (P.vertices (P.succ i)))/2
 
-def symmetricStarshapedPolygon (P : starshapedPolygon) : Prop :=
-  P.n % 2 = 0 ∧
-  ∀ i j : Fin P.n, j = (i + P.n/2) % P.n → P.vertices i = -P.vertices j
+  def admissible (P : starshaped) : Prop :=
+    ∀ i j k l : Fin P.n, j = (i + 1) % P.n ∧ k = (i + 2) % P.n ∧ l = (i + 3) % P.n ∧ ∀ p q : ℤ, Int.gcd p q = 1 →
+    -- check no degenerate vertices
+    (cross (P.vertices j - P.vertices i) (P.vertices k - P.vertices j) ≠ 0) ∧
+    -- check tangencies at vertices
+    (cross (p, q) (rotateRight (P.vertices j - P.vertices i)) < 0 ∧
+    cross (p, q) (rotateRight (P.vertices k - P.vertices j)) > 0 ∨
+    cross (p, q) (rotateRight (P.vertices j - P.vertices i)) > 0 ∧
+    cross (p, q) (rotateRight (P.vertices k - P.vertices j)) < 0
+    → dot (p, q) (P.vertices j) ≥ 1) ∧
+    -- check tangencies at edges
+    (cross (p, q) (rotateRight (P.vertices k - P.vertices j)) = 0 ∧
+    -- check whether tangency is real
+    (cross (p, q) (rotateRight (P.vertices j - P.vertices i)) < 0 ∧
+    cross (p, q) (rotateRight (P.vertices l - P.vertices k)) > 0 ∨
+    cross (p, q) (rotateRight (P.vertices j - P.vertices i)) > 0 ∧
+    cross (p, q) (rotateRight (P.vertices l - P.vertices k)) < 0)
+    → dot (p, q) (P.vertices j) ≥ 1)
 
-def admissiblePolygon (P : starshapedPolygon) : Prop :=
-  ∀ i j k l : Fin P.n, j = (i + 1) % P.n → k = (i + 2) % P.n → l = (i + 3) % P.n → ∀ p q : ℤ, Int.gcd p q = 1 →
-  -- check tangencies at vertices
-  (if angle (p, q) ((P.vertices j).2 - (P.vertices i).2, (P.vertices i).2 - (P.vertices j).2) < 0 ∧
-  angle (p, q) ((P.vertices k).2 - (P.vertices j).2, (P.vertices j).2 - (P.vertices k).2) > 0
-  then p * (P.vertices j).1 + q * (P.vertices j).2 ≥ 1 else true) ∧
-  -- check tangencies at edges
-  (if angle (p, q) ((P.vertices k).2 - (P.vertices j).2, (P.vertices k).2 - (P.vertices j).2) = 0 ∧
-  -- check whether tangecy is real
-  ((angle (p, q) ((P.vertices j).2 - (P.vertices i).2, (P.vertices i).2 - (P.vertices j).2) < 0 ∧
-  angle (p, q) ((P.vertices l).2 - (P.vertices k).2, (P.vertices k).2 - (P.vertices l).2) > 0) ∨
-  (angle (p, q) ((P.vertices j).2 - (P.vertices i).2, (P.vertices i).2 - (P.vertices j).2) > 0 ∧
-  angle (p, q) ((P.vertices l).2 - (P.vertices k).2, (P.vertices k).2 - (P.vertices l).2) < 0))
-  then p * (P.vertices j).1 + q * (P.vertices j).2 ≥ 1 else true)
+  def symmetric (P : starshaped) : Prop :=
+    P.n % 2 = 0 ∧
+    ∀ i j : Fin P.n, j = (i + P.n/2) % P.n → P.vertices i = -P.vertices j
 
-def polygonArea (P : starshapedPolygon) : ℚ :=
-  ∑ i : Fin P.n, ∑ j : Fin P.n, (if j = (i + 1) % P.n then ((P.vertices i).1 * (P.vertices j).2 - (P.vertices i).2 * (P.vertices j).1)/2 else 0)
+  theorem systolicInequality (P : starshaped) (hp : admissible P ∧ symmetric P) :
+      area P ≥ (4 : ℚ) / (3 : ℚ) :=
+    sorry
 
-lemma polygonalApproximation (f : ℝ → ℝ) (hf : symmetricAdmissibleFunction f) :
-    ∀ ε > 0, ∃ P : starshapedPolygon, admissiblePolygon P ∧
-    symmetricStarshapedPolygon P ∧
-    polygonArea P < ε + integrate f :=
-  sorry
+  theorem systolicInequalityIsSharp :
+      ∃ P : starshaped, admissible P ∧ symmetric P ∧
+      area P = (4 : ℚ) / (3 : ℚ) :=
+    sorry
 
-theorem systolicInequalityForPolygons (P : starshapedPolygon) (hp : admissiblePolygon P)(hs : symmetricStarshapedPolygon P) :
-    polygonArea P ≥ (4 : ℚ) / (3 : ℚ) :=
-  sorry
+end polygons
 
-lemma nonOptimality (f : ℝ → ℝ) (hf : symmetricAdmissibleFunction f) :
-    ∃ g : ℝ → ℝ, symmetricAdmissibleFunction g ∧
-    integrate g < integrate f :=
-  sorry
+namespace smoothDomains
+  open Real
 
-theorem systolicInequality (f : ℝ → ℝ) (hf : symmetricAdmissibleFunction f) :
-    integrate f > (4 : ℝ) / (3 : ℝ) :=
-  by
-  -- assume integrate f ≤ 4/3
-  by_contra h
-  rw [not_lt] at h
-  -- use non optimality to get integrate g < 4/3
-  obtain ⟨g, hg⟩ := nonOptimality f hf
-  -- use polygonal approximation to get a polygon p with area < 4/3
-  obtain ⟨P, hp⟩ := polygonalApproximation g hg.1 ((4 : ℝ) / (3 : ℝ) - integrate g) (sub_pos_of_lt (lt_of_lt_of_le hg.2 h))
-  have hcoe : (4 : ℝ) / (3 : ℝ) = ↑((4 : ℚ) / (3 : ℚ)) := (Rat.cast_div (4 : ℚ) (3 : ℚ)).symm
-  rw [sub_add_cancel, hcoe, Rat.cast_lt] at hp
-  -- conclude with the systolic inequality for polygons
-  exact not_le_of_gt hp.2.2 (systolicInequalityForPolygons P hp.1 hp.2.1)
+  noncomputable def angle (u v : ℝ × ℝ) : ℝ :=
+    arccos ((u.1 * v.1 + u.2 * v.2) / (norm u * norm v)) * (if (u.1 * v.2 - u.2 * v.1) < 0 then -1 else 1)
 
+  lemma rightAngle (u v : ℝ × ℝ)(o : u.1 * v.1 + u.2 * v.2 = 0)(p: u.1 * v.2 - u.2 * v.1 > 0) : angle u v = π/2 :=
+    by
+    unfold angle
+    rw [o, zero_div]
+    simp only [Real.arccos_zero]
+    rw [if_neg (not_lt_of_gt p)]
+    norm_num
 
-theorem systolicInequalityIsSharp :
-    ∀ ε > 0, ∃ f : ℝ → ℝ, symmetricAdmissibleFunction f ∧
-    integrate f < (4 : ℝ) / (3 : ℝ) + ε :=
-  sorry
+  def admissible (f : ℝ → ℝ) : Prop :=
+    (ContDiff ℝ (⊤ : ℕ∞) f) ∧
+    (∀ x : ℝ, f x > 0 ∧ f (x + 2*π) = f x) ∧
+    (∀ p q : ℤ, Int.gcd p q = 1 → (∀ x : ℝ, angle (p, q) (cos x * deriv f x - sin x * f x, sin x * deriv f x + cos x * f x) = π/2 → (p * cos x + q * sin x) * f x ≥ 1))
+
+  def symmetric (f : ℝ → ℝ) : Prop :=
+    ∀ x : ℝ, f (x + π) = f x
+
+  noncomputable def integrate (f : ℝ → ℝ) : ℝ :=
+    ∫ (x : ℝ) in (0 : ℝ)..2 * Real.pi, ((f x)^2)/2
+
+  lemma polygonalApproximation (f : ℝ → ℝ) (hf : admissible f ∧ symmetric f) :
+      ∀ ε > 0, ∃ P : polygons.starshaped, polygons.admissible P ∧ polygons.symmetric P ∧
+      polygons.area P < ε + integrate f :=
+    sorry
+
+  lemma nonOptimality (f : ℝ → ℝ) (hf : admissible f ∧ symmetric f) :
+      ∃ g : ℝ → ℝ, admissible g ∧ symmetric g ∧
+      integrate g < integrate f :=
+    sorry
+
+  theorem systolicInequality (f : ℝ → ℝ) (hf : admissible f ∧ symmetric f) :
+      integrate f > (4 : ℝ) / (3 : ℝ) :=
+    by
+    -- assume integrate f ≤ 4/3
+    by_contra h
+    rw [not_lt] at h
+    -- use non optimality to get integrate g < 4/3
+    obtain ⟨g, hg⟩ := nonOptimality f hf
+    -- use polygonal approximation to get a polygon p with area < 4/3
+    obtain ⟨P, hp⟩ := polygonalApproximation g (And.intro hg.1 hg.2.1) ((4 : ℝ) / (3 : ℝ) - integrate g) (sub_pos_of_lt (lt_of_lt_of_le hg.2.2 h))
+    have hcoe : (4 : ℝ) / (3 : ℝ) = ↑((4 : ℚ) / (3 : ℚ)) := (Rat.cast_div (4 : ℚ) (3 : ℚ)).symm
+    rw [sub_add_cancel, hcoe, Rat.cast_lt] at hp
+    -- conclude with the systolic inequality for polygons
+    exact not_le_of_gt hp.2.2 (polygons.systolicInequality P (And.intro hp.1 hp.2.1))
+
+  theorem systolicInequalityIsSharp :
+      ∀ ε > 0, ∃ f : ℝ → ℝ, admissible f ∧ symmetric f ∧
+      integrate f < (4 : ℝ) / (3 : ℝ) + ε :=
+    sorry
+
+end smoothDomains
